26eecf29773472b5f5f7a747d473053123eb1a83
Jaap Wiering <github.com@fanagalo.nl>
2023-04-27 12:58:23 +0200

    initial commit

---
+++ functions.php
@@ -0,0 +1,10 @@
+/* some comment */
+
+<?php
+add_action(
+  'wp_enqueue_scripts',
+  function () {
+     wp_enqueue_style('parent-style', get_stylesheet_directory_uri() . '/style.css');
+  },
+  10
+);
